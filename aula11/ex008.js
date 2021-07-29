9b84fda9c919b99251ae364a520c0edd45b96c17
Huann Almeida <huannvictor@gmail.com>
2021-07-29 16:56:19 -0300

    novos arquivos

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59.5
+console.log(`a velocidade é de ${vel}km/h`)
+if (vel > 60){ //condição simples
+    console.log(`você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log('Dirija sempre usando o cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'BRA'
+console.log(`vivendo em ${pais}`)
+if(pais == 'BRA'){
+    console.log('você é brasileiro')
+}else{
+    console.log('você é estrangeiro')
+}
