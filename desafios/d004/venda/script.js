eacb6fbe18011dcf745d83925b8db1b882e532c3
Huann Almeida <huannvictor@gmail.com>
2021-08-02 10:26:30 -0300

    resoluções calculo saída de vendas

--- desafios/d004/venda/script.js
+++ desafios/d004/venda/script.js
@@ -1,25 +1,26 @@
 
 function calcular(){
-    var txtprod = prompt('produto comprado:')
-    var txtcost = prompt(`qual o valor do produto: ${txtprod}?`)
-    var txtrece = prompt(`valor recebido pelo item ${txtprod}:`)
+    //formulario inclusão de dados
+    let txtprod = document.getElementById('txtprod')
+    let txtcost = document.getElementById('txtcost')
+    let txtrece = document.getElementById('txtrece')
 
-    alert = (`${txtprod}`)
-    
-    let cost = Number(txtcost)
-    let rece = Number(txtrece)
+    //tratamento dos dados
+    let prod = String(txtprod.value)
+    let cost = Number(txtcost.value)
+    let rece = Number(txtrece.value)
     let troc = rece - cost
 
+    //resposta - recibo
     let item = document.getElementById('item')
-    item.innerHTML = ` ${txtprod}`
+    item.innerHTML = ` ${prod}`
 
     let vlritem = document.getElementById('vlritem')
-    vlritem.innerHTML = ` ${cost.toLocaleString('pt-BR',{style: 'currency', currency:'BRL'})}`
+    vlritem.innerHTML = `${cost.toLocaleString('pt-BR',{style:'currency', currency:'BRL'})}`
 
     let vlrpago = document.getElementById('vlrpago')
-    vlrpago.innerHTML = `${rece.toLocaleString('pt-BR',{style: 'currency', currency:'BRL'})}`
+    vlrpago.innerHTML = `${rece.toLocaleString('pt-BR',{style:'currency', currency:'BRL'})}`
 
-    let torco = document.getElementById('torco')
-    torco.innerHTML = `${troc.toLocaleString('pt-BR',{style: 
-    'currency', currency:'BRL'})}`
+    let vlrtroco = document.getElementById('vlrtroco')
+    vlrtroco.innerHTML = `${troc.toLocaleString('pt-BR',{style:'currency', currency:'BRL'})}`
 }
