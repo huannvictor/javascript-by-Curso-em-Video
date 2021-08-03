48cd9842452f7765d506250cbf1b6b14e518a421
Huann Almeida <huannvictor@gmail.com>
2021-08-02 23:57:21 -0300

    Exer Conv. de med sem promtp. Input direto na pag

--- huann/conversorDeMedidas/script.js
+++ huann/conversorDeMedidas/script.js
@@ -1,21 +1,35 @@
 function calcular(){
-    var txtprod = prompt('produto comprado:')
-    var txtcost = prompt(`qual o valor do produto: ${txtprod}?`)
-    var txtrece = prompt(`valor recebido pelo item ${txtprod}:`)
+    let txtmet = document.getElementById('txtmet')
+    let met = Number(txtmet.value)
    
-    var cost = Number(txtcost)
-    var rece = Number(txtrece)
-    var troc = rece - cost
+    //conversões
+    let km = met/1000
+    let hm = met/100
+    let dam = met/10
+    let dm = met*10
+    let cm = met*100
+    let mm = met*1000
 
-    let item = document.getElementById('item')
-    item.innerHTML = ` ${txtprod}`
+    //saída
 
-    let vlritem = document.getElementById('vlritem')
-    vlritem.innerHTML = ` ${cost.toLocaleString('pt-BR',{style: 'currency', currency:'BRL'})}`
+    let smet = document.getElementById('met')
+    smet.innerHTML = `<strong> ${met} m metro(s)</strong>`
 
-    let vlrpago = document.getElementById('vlrpago')
-    vlrpago.innerHTML = `${rece.toLocaleString('pt-BR',{style: 'currency', currency:'BRL'})}`
+    let skm = document.getElementById('km')
+    skm.innerHTML = `${km} km quilômetro(s)`
 
-    let torco = document.getElementById('torco')
-    torco.innerHTML = `${troc.toLocaleString('pt-BR',{style: 'currency', currency:'BRL'})}`
+    let shm = document.getElementById('hm')
+    shm.innerHTML = ` ${hm} hm hectômetro(s)`
+
+    let sdam = document.getElementById('dam')
+    sdam.innerHTML = `${dam} Dam Decâmetro(s)`
+
+    let sdm = document.getElementById('dm')
+    sdm.innerHTML = ` ${dm} dm decímetro(s)`
+
+    let scm = document.getElementById('cm')
+    scm.innerHTML = `${cm} cm centímetro(s)`
+
+    let smm = document.getElementById('mm')
+    smm.innerHTML = `${mm} mm milímetro(s)`
 }
