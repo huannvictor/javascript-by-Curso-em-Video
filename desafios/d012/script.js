98b38a76ae2e9d6d0eaaaf569b1649fc340968ab
Huann Almeida <huannvictor@gmail.com>
2021-08-07 15:53:36 -0300

    iniciado desafio 12

---
+++ desafios/d012/script.js
@@ -0,0 +1,7 @@
+function verificar(){
+    let prod = prompt('qual o produto?')
+    let valor = [
+        Number(prompt(`qual o preço anterior do ${prod}?`)),
+        Number(prompt(`qual o valor atual do ${prod}?`))
+    ]
+}
