499e50179d661b6cb863315599f9452a82ce1b9c
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-09-19 08:37:25 -0300

    update desafio 12

--- desafios/d012/script.js
+++ desafios/d012/script.js
@@ -1,7 +1,24 @@
 function verificar(){
+    //entrada
     let prod = prompt('qual o produto?')
     let valor = [
-        Number(prompt(`qual o preço anterior do ${prod}?`)),
-        Number(prompt(`qual o valor atual do ${prod}?`))
+         Number(prompt(`qual o preço anterior do ${prod}?`)),
+         Number(prompt(`qual o valor atual do ${prod}?`))
     ]
+    
+    //saída
+    let saida = (document.querySelector('div#schild')).innerHTML = 
+    `
+    <h2>Alteração do preço de ${prod}</h2>
+    <table>
+        <tr>
+            <td>Valor anterior:</td>
+            <td>${valor[0].toLocaleString('pt-br',{style:'currency', currency:'BRL'})}</td>
+        </tr>
+        <tr>
+            <td>Valor atual:</td>
+            <td>${valor[1].toLocaleString('pt-br',{style:'currency',currency:'BRL'})}</td>
+        </tr>
+    </table>
+    `
 }
