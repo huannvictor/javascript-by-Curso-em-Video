baad71407dd45e8cdd379fb6ec7e9f177bb578b9
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-09-19 19:13:43 -0300

    ajuste no arquivo css padrão + ex010 resolvido

---
+++ exercicios/ex010/sricpt.js
@@ -0,0 +1,17 @@
+saida = document.querySelector('div#sparent')
+
+function acao1(){
+    saida.innerHTML += `<p>Clicou no 1º botão</p>`
+}
+function acao2(){
+    saida.innerHTML += `<p>Clicou no 2º botão</p>`
+}
+function acao3(){
+    saida.innerHTML += `<p>Clicou no 3º botão</p>`
+}
+function acao4(){
+    saida.innerHTML += `<p>Clicou no 4º botão</p>`
+}
+function zerar(){
+    saida.innerHTML = '<P>Aqui vamos registrar suas ações com os botões acima</P>'
+}
