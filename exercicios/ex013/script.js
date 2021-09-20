68b4e32a7e58034e1f00d3c777a4d0f4664a6a1e
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-09-19 21:52:57 -0300

    ex013 resolvido

---
+++ exercicios/ex013/script.js
@@ -0,0 +1,16 @@
+function calcular (){
+    //entrada
+    let valor = [
+        Number(prompt('digite o primeiro número:')),
+        Number(prompt('digite o segundo número:'))
+    ]
+
+    //calculo
+    if (valor[0] > valor[1]){
+        saida = (document.querySelector('div#sparent')).innerHTML=`
+        <p>Analisanso os valores ${valor[0]} e ${valor[1]}, o maior valor é ${valor[0]} </p>`
+    }else{
+        saida = (document.querySelector('div#sparent')).innerHTML=`
+        <p>Analisanso os valores ${valor[0]} e ${valor[1]}, o maior valor é ${valor[1]} </p>`
+    }
+}
