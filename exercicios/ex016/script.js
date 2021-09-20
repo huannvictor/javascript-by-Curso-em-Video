7fb17049bf1c6354e4b829120c81d6ed864b942a
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-09-20 10:27:08 -0300

    código ex016 simplificado

--- exercicios/ex016/script.js
+++ exercicios/ex016/script.js
@@ -1,7 +1,13 @@
-function iniciar(){
-    let entrada = Number(prompt('Insira seu ano de nascimento:'))
+    /*
     let data = new Date
     let ano = data.getFullYear()
+
+    subistituído por: let ano = new Date().getFullYear()
+    */
+
+function iniciar(){
+    let entrada = Number(prompt('Insira seu ano de nascimento:'))
+    let ano = new Date().getFullYear()
 
     //calculo
     let idade = ano - entrada
