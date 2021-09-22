0d71d6084797a94444cf7a0a6aabed058c8a9af2
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-09-22 15:07:30 -0300

    ex026 resolvido + adc margin na div no css padrão

---
+++ exercicios/ex026/main.js
@@ -0,0 +1,25 @@
+function contar(){
+    let cont = [
+        0,
+        Number(document.querySelector('input#inicial').value),
+        Number(document.querySelector('input#final').value)
+    ]
+    let saida = document.querySelector('div#saida')
+
+    if (cont[1] < cont[2]){
+        cont[0] = cont[1]
+        while (cont[0] <= cont[2]){
+            saida.innerHTML += `${cont[0]} →`
+            cont[0] ++
+        }
+    }else if (cont[1] > cont[2]){
+        cont[0] = cont[1]
+        while (cont[0] >= cont[2]){
+            saida.innerHTML += `${cont[0]} →`
+            cont[0] --
+        }
+    }else{
+        saida.innerHTML += 'Não é possível contar porque os números são iguais'
+    }
+    saida.innerHTML += `🏁<br>`
+}
