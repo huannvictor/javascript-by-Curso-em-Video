3664fc0b78d97cfb41380a45b252559cbeb5bbfd
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-09-25 11:59:48 -0300

    avanço ex final + melhorias no style padrão

--- exercicios/exercicio final/main.js
+++ exercicios/exercicio final/main.js
@@ -1,7 +1,32 @@
+
+let tarea = document.querySelector('textarea#saida')
+let saida = document.querySelector('div#saida')
+let valor = []
+let cont = 0
+let soma = 0
+let scont = 1
+
 function adicionar(){
     let num = Number(document.querySelector('input#num').value)
-    let tarea = document.querySelector('textarea#saida')
+    valor.push(num)
     tarea.innerHTML += 
     `O valor ${num} foi adicionado.
 `
 }
+function finalizar(){
+    saida.innerHTML += `<p>Ao todo temos ${valor.length} valores adicionados</p>`
+    for (let pos in valor){
+        saida.innerHTML += `${valor[pos]} `
+    }
+    soma = valor[0] + valor[1]
+    saida.innerHTML += `<br> a soma é ${soma}`
+
+}
+
+/*
+total valores
+maior
+menor
+soma
+média
+*/
