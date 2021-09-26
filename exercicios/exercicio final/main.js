0c49515b7837d0fb47518ebf84837cb1704297f8
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-09-26 14:29:40 -0300

    progress in final exercice

--- exercicios/exercicio final/main.js
+++ exercicios/exercicio final/main.js
@@ -1,32 +1,29 @@
-
+let num = document.querySelector('input#num')
 let tarea = document.querySelector('textarea#saida')
 let saida = document.querySelector('div#saida')
-let valor = []
-let cont = 0
-let soma = 0
-let scont = 1
+let valores = []
+
+function intervalo (n){
+    if (Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista (n, l){
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
 
 function adicionar(){
-    let num = Number(document.querySelector('input#num').value)
-    valor.push(num)
-    tarea.innerHTML += 
-    `O valor ${num} foi adicionado.
+    if (intervalo(num.value) && !inLista(num.value, valores)){
+        tarea.innerHTML += `O valor ${num.value} foi adicionado
 `
-}
-function finalizar(){
-    saida.innerHTML += `<p>Ao todo temos ${valor.length} valores adicionados</p>`
-    for (let pos in valor){
-        saida.innerHTML += `${valor[pos]} `
+    }else{
+        window.alert('valor fora do intervalo permitido ou já está na lista')
     }
-    soma = valor[0] + valor[1]
-    saida.innerHTML += `<br> a soma é ${soma}`
-
 }
-
-/*
-total valores
-maior
-menor
-soma
-média
-*/
