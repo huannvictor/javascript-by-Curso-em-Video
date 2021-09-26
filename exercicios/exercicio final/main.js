df072d79b2c04c253039a2e05ac05fe35743c9a6
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-09-26 15:51:44 -0300

    final exercise solved

--- exercicios/exercicio final/main.js
+++ exercicios/exercicio final/main.js
@@ -1,6 +1,6 @@
 let num = document.querySelector('input#num')
-let tarea = document.querySelector('textarea#saida')
-let saida = document.querySelector('div#saida')
+let tarea = document.querySelector('div#tarea')
+let resultado = document.querySelector('div#resultado')
 let valores = []
 
 function intervalo (n){
@@ -21,9 +21,44 @@ function inLista (n, l){
 
 function adicionar(){
     if (intervalo(num.value) && !inLista(num.value, valores)){
-        tarea.innerHTML += `O valor ${num.value} foi adicionado
-`
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text += `Valor ${num.value} adicionado`
+        tarea.appendChild(item)
+        resultado.innerHTML = ''
     }else{
         window.alert('valor fora do intervalo permitido ou já está na lista')
+    }
+    num.value =''
+    num.focus()
+}
+
+function finalizar(){
+    if (valores.length == 0){
+        window.alert('insira os valores')
+    }else{
+        //valor total
+        let total = valores.length
+        //mair e menor valores
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        for (let pos in valores){
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        let media = soma/total
+
+        //saida
+        resultado.innerHTML = ''
+        resultado.innerHTML += `<p>${total} - total de números cadastrados</p>`
+        resultado.innerHTML += `<p>${maior} - é o maior valor cadastrado</p>`
+        resultado.innerHTML += `<p>${menor} - é o menor cadastrado</p>`
+        resultado.innerHTML += `<p>${soma} - é a soma de todos os valores cadastrados</P>`
+        resultado.innerHTML += `<p>${media} - é a média de todos os valores cadastrados</p>`
+
     }
 }
